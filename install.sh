c292d6f467ba9674adfed5270cb72b695d6797d8
AghayeCoder <ahmand@vk.com>
2025-02-04 19:53:05 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -332,7 +332,7 @@ install_x-ui() {
         min_version="0.1"
 
         if [[ "$(printf '%s\n' "$min_version" "$tag_version_numeric" | sort -V | head -n1)" != "$min_version" ]]; then
-            echo -e "${red}Please use a newer version (at least v2.3.5). Exiting installation.${plain}"
+            echo -e "${red}Please use a newer version (at least v0.1). Exiting installation.${plain}"
             exit 1
         fi
 
